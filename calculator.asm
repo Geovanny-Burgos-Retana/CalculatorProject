1ae8f6588f6fd991d59e3106eabd66f2398febb6
Francisco <geoburgosretana@gmail.com>
2017-04-05 00:22:55 -0600

    Agregue las partes basicas para iniciar codigo en el .asm

---
+++ calculator.asm
@@ -0,0 +1,16 @@
+;Hex equivalent of characters              HEX1CHAR.ASM
+;        Objective: To print the hex equivalent of
+;                   ASCII character code.
+;            Input: Requests a character from the user.
+;           Output: Prints the ASCII code of the
+;                   input character in hex.
+%include "io.mac"
+
+.DATA
+
+.UDATA
+
+.CODE
+    .STARTUP
+    
+    .EXIT
