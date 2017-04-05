9ef7eb2977d031a1639bf670b902e47a8be4d7f5
Francisco <geoburgosretana@gmail.com>
2017-04-05 10:05:47 -0600

    Agregue algunos mensajes en el .DATA

--- calculator.asm
+++ calculator.asm
@@ -1,16 +1,23 @@
-;Hex equivalent of characters              HEX1CHAR.ASM
-;        Objective: To print the hex equivalent of
-;                   ASCII character code.
-;            Input: Requests a character from the user.
-;           Output: Prints the ASCII code of the
-;                   input character in hex.
+;Calculadora entre bases numericas              CALCULATOR.ASM
+;        Objective: Imprimir el  resultado en de una expresion
+;                   ingresada por el usuario especificando las
+;                   bases numericas de cada operando como tam-
+;                   bien en base se debe mostrar el resultado.
+;            Input: Requiere una expresion del usuarrio.
+;           Output: Imprimir resultado de la expresion mostrando
+;                   o no el procedimiento
 %include "io.mac"
 
 .DATA
+msgDivCero      db  "Error!! Ocurrio una division entre 0",0
+msgOverflow     db  "Error!! Ocurrio un overflow",0
+msgSintaxis     db  "Error!! Mal redaccion de la expresion",0
+msgVariable     db  "Error!! Ingreso variables en la expresion",0
 
 .UDATA
+exprexion       resb 30
 
 .CODE
-    .STARTUP
+    .STARTUP    
     
     .EXIT
